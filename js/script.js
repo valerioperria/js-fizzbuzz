fb98e664fa98cd4b3cfbc005fb05f97076fa90fa
valerioperria <valerio.perria@gmail.com>
2023-10-20 14:44:54 +0200

    feat: bonus

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+const rowElem = document.querySelector(".row");
+console.log(rowElem);
+
+let part = "";
 
 // ciclo for per la stampa della lista da 1 a 100 in console
 for (let i = 1; i <= 100; i++) {
@@ -20,4 +24,10 @@ for (let i = 1; i <= 100; i++) {
     }
 
     console.log(result)
+
+    part += `<div class="square ${result}">${result}</div>`
+
+    console.log(`iterazione ${i}`, part);
 }
+
+rowElem.innerHTML = part;
